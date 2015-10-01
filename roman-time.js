640a1c51467f5237fcfd99cc0259f02e64591663
Vedmax <max.vedernikov@gmail.com>
2015-10-02 00:50:02 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -1,4 +1,29 @@
+/**
+ * Created by Max on 01.10.2015.
+ */
 var hours = process.argv[2];
 var minutes = process.argv[3];
+var romanDigits = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+var romanDecims = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
 
-// Немного замечательного кода и магии
+function parseNumber(number)
+{
+    if (number === "00")
+        return "--";
+    var digits = number % 10;
+    var decims = (number - digits)/ 10;
+    return romanDecims[decims] + romanDigits[digits];
+}
+
+function main()
+{
+    if (hours > 23 || minutes > 59 || hours < 0 || minutes < 0)
+    {
+        console.log('Время указано неверно');
+        return;
+    }
+    res = parseNumber(hours) + ':' + parseNumber(minutes);
+    console.log(res);
+}
+
+main();
