2f49ceddb248604fc1e432960dce17cc90d6ce7e
Vedmax <max.vedernikov@gmail.com>
2015-10-04 13:55:30 +0500

    Update roman-time.js[2]
    Added ASCII graphic

--- roman-time.js
+++ roman-time.js
@@ -3,6 +3,141 @@
  */
 var hours = process.argv[2];
 var minutes = process.argv[3];
+
+var romanGraphicDigits = [
+    [
+        '',
+        '',
+        '',
+        '',
+        ''
+    ],
+    [
+        '.___ ',
+        '|   |',
+        '|   |',
+        '|   |',
+        '|___|'
+    ],
+    [
+        '.___ .___ ',
+        '|   ||   |',
+        '|   ||   |',
+        '|   ||   |',
+        '|___||___|'
+    ],
+    [
+        '.___ .___ .___ ',
+        '|   ||   ||   |',
+        '|   ||   ||   |',
+        '|   ||   ||   |',
+        '|___||___||___|'
+    ],
+    [
+        '._______   ____',
+        '|   \\   \\ /   /',
+        '|   |\\   Y   / ',
+        '|   | \\     /  ',
+        '|___|  \\___/   '
+    ],
+    [
+       '____   ____',
+       '\\   \\ /   /',
+       ' \\   Y   / ',
+       '  \\     /  ',
+       '   \\___/   '
+    ],
+    [
+        '____   ____.___ ',
+        '\\   \\ /   /|   |',
+        ' \\   Y   / |   |',
+        '  \\     /  |   |',
+        '   \\___/   |___|'
+    ],
+    [
+        '____   ____.___ .___ ',
+        '\\   \\ /   /|   ||   |',
+        ' \\   Y   / |   ||   |',
+        '  \\     /  |   ||   |',
+        '   \\___/   |___||___|'
+    ],
+    [
+        '____   ____.___ .___ .___ ',
+        '\\   \\ /   /|   ||   ||   |',
+        ' \\   Y   / |   ||   ||   |',
+        '  \\     /  |   ||   ||   |',
+        '   \\___/   |___||___||___|'
+    ],
+    [
+        '._______  ___',
+        '|   \\   \\/  /',
+        '|   |\\     / ',
+        '|   |/     \\ ',
+        '|___/___/\\__\\'
+    ]
+];
+
+var graphicColon = [
+    '    ',
+    ' /\\ ',
+    ' \\/ ',
+    ' /\\ ',
+    ' \\/ '
+];
+
+var graphicZero = [
+    '         ',
+    '         ',
+    '  ______ ',
+    '         ',
+    '         '
+];
+
+var romanGraphicDecims = [
+    [
+        '',
+        '',
+        '',
+        '',
+        ''
+    ],
+    [
+        '____  ___',
+        '\\   \\/  /',
+        ' \\     / ',
+        ' /     \\ ',
+        '/___/\\__\\'
+    ],
+    [
+        '____  _______  ___',
+        '\\   \\/  /\\   \\/  /',
+        ' \\     /  \\     / ',
+        ' /     \\  /     \\ ',
+        '/___/\\__\\/___/\\__\\'
+    ],
+    [
+        '____  _______  _______  ___ ',
+        '\\   \\/  /\\   \\/  /\\   \\/  / ',
+        ' \\     /  \\     /  \\     /  ',
+        ' /     \\  /     \\  /     \\  ',
+        '/___/\\__\\/___/\\__\\/___/\\__\\'
+    ],
+    [
+        '____  ___.____     ',
+        '\\   \\/  /|    |    ',
+        ' \\     / |    |    ',
+        ' /     \\ |    |___ ',
+        '/___/\\__\\|________|'
+    ],
+    [
+        '.____     ',
+        '|    |    ',
+        '|    |    ',
+        '|    |___ ',
+        '|________|'
+    ]
+];
+
 var romanDigits = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
 var romanDecims = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
 
@@ -15,15 +150,39 @@ function parseNumber(number)
     return romanDecims[decims] + romanDigits[digits];
 }
 
+function draw(hours, minutes)
+{
+    var hoursDigits = romanGraphicDigits[hours % 10];
+    var hoursDecims = romanGraphicDecims[(hours - (hours % 10)) / 10];
+    var minutesDigits = romanGraphicDigits[minutes % 10];
+    var minutesDecims = romanGraphicDecims[(minutes - (minutes % 10)) / 10];
+
+    if (hours == '00'){
+        hoursDigits = hoursDecims = graphicZero;
+    }
+    if (minutes == '00'){
+        minutesDigits = minutesDecims = graphicZero;
+    }
+
+    for (var i = 0; i < 5; i++)
+    {
+        console.log(hoursDecims[i], hoursDigits[i], graphicColon[i],
+            minutesDecims[i], minutesDigits[i]);
+    }
+}
+
 function main()
 {
     if (hours > 23 || minutes > 59 || hours < 0 || minutes < 0)
     {
-        console.log('Время указано неверно');
+        console.log('Время указано не верно');
         return;
     }
-    res = parseNumber(hours) + ':' + parseNumber(minutes);
+
+    var res = parseNumber(hours) + ':' + parseNumber(minutes);
     console.log(res);
+
+    draw(hours, minutes);
 }
 
 main();
